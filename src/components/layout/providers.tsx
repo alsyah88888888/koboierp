fa5cb6f4be59eeb57d7b3950ded5a0ea94b07413
alsyah88888888 <lubisr78@gmail.com>
2026-04-03 10:30:05 +0700

    fix

--- src/components/layout/providers.tsx
+++ src/components/layout/providers.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+import dynamic from "next/dynamic";
 import { Toaster } from "react-hot-toast";
 import { SidebarProvider } from "./SidebarContext";
 
 import { Session } from "next-auth";
+
+const SessionProvider = dynamic(() => import("next-auth/react").then(mod => mod.SessionProvider), { 
+    ssr: false,
+    loading: () => <>{null}</> 
+});
 
 export function Providers({ children, session }: { children: React.ReactNode, session?: Session | null }) {
     return (
